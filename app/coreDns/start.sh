d04e78e3e15cda3cacc6fb220a0a79c7d1aa250b
iasent <rkdrldn@koreatech.ac.kr>
2022-02-21 14:51:08 +0900

    t2

---
+++ app/coreDns/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BASE_DIR=/home/pi/app
+APP_DIR=$BASE_DIR/coreDns
+
+sudo $APP_DIR/coredns -conf $APP_DIR/Corefile
--- coreDns/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-APP_DIR=/home/pi/app
-COREDNS_DIR=$APP_DIR/coreDns
-
-sudo $COREDNS_DIR/coredns -conf $COREDNS_DIR/Corefile
